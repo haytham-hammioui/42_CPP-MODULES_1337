ad567b8550ff50431c6a4d44a09909206e825a75
haytham-hammioui <haythamhammioui16@gmail.com>
2026-05-14 15:57:41 +0100

    life is life lalalala

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <algorithm>
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+
+template <typename T>
+typename T::iterator easyfind(T &container, int value)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+    if (it != container.end())
+        return it;
+    throw std::runtime_error("Value not found in container");
+}
---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,20 @@
+#include "easyfind.hpp"
+
+int main()
+{
+    std::vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+
+    try {
+        std::vector<int>::iterator it = easyfind(vec, 10);
+        std::cout << "Value found: " << *it << std::endl;
+    } 
+    catch (const std::runtime_error &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    return 0;
+}
